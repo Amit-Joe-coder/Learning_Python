7ff231903a8831a7c0e6bdc6152f2c5f4ca8f072
Amit Joe <joe.amit@yahoo.com>
2022-12-05 10:00:42 +0530

    Create Repl_p1.py
    In this you get to know the dataTypes in Python

---
+++ Repl_p1.py
@@ -0,0 +1,16 @@
+#In python everythingh is object
+#int type
+a=1
+print(type(a))
+#float type
+b=4.908
+print(type(b))
+#bool type
+c=True
+print(type(c))
+#dict: it is mapped type the are immutable
+d={'1':"contry","Nation":"India"}
+print(type(d))
+#list it is a collection data they are mutable
+e=[1,'amit',5.4,["True",6]]
+print(type(e))
